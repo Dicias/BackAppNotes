5d4226051a9da30f2ddd659c8d9b9d739e160186
dicias <dmacias26@alumnos.uaq.mx>
2023-06-25 21:09:51 -0600

    changes public dir

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('public'))
 const requestLogger = (request,response,next) =>{
     console.log('Method:' , request.method)
     console.log('Path: ' ,request.path)
