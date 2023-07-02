1fa1a81c83db1ed0a0fb66901d215250c0309365
dicias <dmacias26@alumnos.uaq.mx>
2023-07-02 16:16:25 -0600

    models

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+//manejador Backend
 const express = require('express')
 const app = express()
 app.use(express.json())
+//Para usar el archivo env
+require('dotenv').config()
+//Permite origenes distintos
 const cors = require('cors')
 app.use(cors())
+//Permite usar la DB
+const mongoose = require('mongoose')
+//Llamamos al modulo
+const Note = require('./models/note')
+const note = require('./models/note')
+//Para desplegar archivos estaticos (Vercel lo hace solo, con la config correcta del archivo)
 app.use(express.static('public'))
+//Para mostrar más datos en el Log 
 const requestLogger = (request,response,next) =>{
     console.log('Method:' , request.method)
     console.log('Path: ' ,request.path)
@@ -12,7 +23,6 @@ const requestLogger = (request,response,next) =>{
     next()
 }
 app.use(requestLogger)
-
 
 let notes = [
     {
@@ -34,59 +44,95 @@ let notes = [
       important: true
     }
 ]
+const url =process.env.MONGODB_URI
+mongoose.connect(url)
+
 app.get('/', (request, response)=>{
     response.send('Hello World')
 })
 
-app.get('/api/notes', (request, response)=>{
-    response.json(notes)
-})
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
+  })
 
-app.get('/api/notes/:id', (request,response)=>{
-    const id = Number(request.params.id)
-    const note = notes.find(note=>note.id===id)
-    if(note){
+app.get('/api/notes/:id', (request,response,next)=>{
+    Note.findById(request.params.id)
+    .then(note=>{
+        if(note){
         response.json(note)
-    }else{
-        response.status(404).end()
-    }
+        }else{
+            response.status(404).end()
+        }
+    })
+    .catch(error=>{
+        console.log(error)
+        next(error)
+    })
 })
 
-app.delete('/api/notes/:id', (request,response)=>{
-    const id = Number(request.params.id)
-    notes = notes.filter(note=>note.id!==id)
-    response.status(204).end()
+app.delete('/api/notes/:id', (request,response, next)=>{
+    Note.findByIdAndDelete(request.params.id)
+    .then(res=> {
+        response.json(204).end()
+    }).catch(error => next(error))
 })
 
-const generateId = () =>{
-    const maxId = notes.length>0? Math.max(...notes.map(n=>n.id)):0
-    return maxId+1
-}
-app.post('/api/notes', (request,response)=>{
+
+app.post('/api/notes', (request,response, next)=>{
     const body = request.body
 
-if(!body.content){
-    return response.status(400).json({
-        error: 'content missing'
-    })
-}
-const note = {
+const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-}
-
-notes = notes.concat(note)
-response.json(note)
 })
+note
+.save()
+.then(savedNote=>savedNote.toJSON())
+.then(savedAndFormattedNote =>{
+    response.json(savedAndFormattedNote)
+    })
+.catch(error => next(error))
+
+})
+
+app.put('/api/notes/:id', (request, response, next)=>{
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important:body.important
+    }
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updateNote=>{
+        response.json(updateNote)
+    }) 
+    .catch(error=> next(error))
+})
+
 
 const unknownEndpoint = (request, response) =>{
     response.status(404).send({error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next)=>{
+console.error(error.message)
+
+if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+}else if(error.name === 'ValidationError'){
+    return response.status(400).send({error: error.message})
+}
+next(error)
+}
+app.use(errorHandler)
+
+
+
+const PORT = process.env.PORT
 
 app.listen(PORT, ()=>{
     console.log('Server running on port 3001')
